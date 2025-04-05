4bef238312b3ccf37462c6e450e59165e28e699e
Sebastian Hutter <sebastian.hutter@rustyquill.com>
2025-04-05 22:57:44 +0200

    chore: export classes

--- zitadel_authorizer/__init__.py
+++ zitadel_authorizer/__init__.py
@@ -1,5 +1,10 @@
 from .authorizer import Authorizer
 from .middleware import ScopeMiddleware
 from .introspector import Introspector
-from .models import ApplicationKey, IntrospectionResponse, IntrospectorSettings
+from .models import (
+    ApplicationKey,
+    IntrospectionResponse,
+    IntrospectorSettings,
+    AuthorizerSettings,
+)
 from .helper import get_bearer_token_from_aws_gateway_authorizer_event
