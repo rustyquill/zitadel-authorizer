a7cc85256a388fada40a4561f943c8e0cc728b32
Sebastian Hutter <sebastian.hutter@rustyquill.com>
2025-04-06 10:25:41 +0200

    feat: add service endpoints to test the middlewares

--- example/lambda/service/index.py
+++ example/lambda/service/index.py
@@ -1,22 +1,54 @@
-import requests
-from requests import Response
-
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from zitadel_authorizer.middleware import (
+    IsAuthenticatedMiddleware,
+    ProjectRoleAuthorizationMiddleware,
+)
 
 logger = Logger()
-app = APIGatewayHttpResolver()
+app = APIGatewayHttpResolver(strip_prefixes=["/public", "/private"])
 
 
-@app.get("/todos")
-def get_todos():
-    todos: Response = requests.get("https://jsonplaceholder.typicode.com/todos")
-    todos.raise_for_status()
+# this endpoint can be accessed by anyone
+# of course if you're protecting the service lambda with a gateway authorizer
+# the authorizer will be invoked first!
+@app.get("/anyone")
+def anyone():
+    """
+    This endpoint is open to anyone
+    """
 
-    # for brevity, we'll limit to the first 10 only
-    return {"todos": todos.json()[:10]}
+    return {
+        "message": "Anyone can access this endpoint",
+    }
+
+
+# this endpoint is only accessible for authenticated users
+# e.g. the isauthenticated middleware checks if the authorizer context is present
+@app.get("/authenticated", middlewares=[IsAuthenticatedMiddleware()])
+def authenticated():
+    """
+    This endpoint is only accessible for authenticated users
+    """
+
+    return {
+        "message": "Authenticated users can access this endpoint",
+    }
+
+
+# this endpoint is only accessible for users with the project role "demo"
+# you can create a project role in zitadel and assign it to a user!
+@app.get("/demo", middlewares=[ProjectRoleAuthorizationMiddleware(roles=["demo"])])
+def demo():
+    """
+    This endpoint is only accessible for users with the project role "demo"
+    """
+
+    return {
+        "message": "Users with the project role 'demo' can access this endpoint",
+    }
 
 
 # You can continue to use other utilities just as before
